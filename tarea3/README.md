1cdccbd282ce69bbaf21e30193e89889fca284b3
acabe10 <alejandrocabezab@gmail.com>
2021-02-26 00:57:18 +0100

    subo ejercicio 3 sin completar

--- tarea3/README.md
+++ tarea3/README.md
@@ -1,4 +1,4 @@
-# Tarea 3: Ejecución de una aplicación PHP en dockerPermalink
+# Tarea 3: Ejecución de una aplicación PHP en docker
 
 * En este caso queremos usar un contenedor que utilice nginx para servir la aplicación PHP. Puedes crear la imagen desde una imagen base debian o ubuntu o desde la imagen oficial de nginx.
 * Vamos a crear otro contenedor que sirva php-fpm.
--- tarea3/build/script.sh
+++ tarea3/build/script.sh
@@ -3,4 +3,3 @@
 sed -i "s/$this->user=\"root\";/$this->user=\"$DATABASE_USER\";/g" /var/www/html/core/controller/Database.php
 sed -i "s/$this->pass=\"\";/$this->pass=\"$DATABASE_PASSWORD\";/g" /var/www/html/core/controller/Database.php
 sed -i "s/$this->host=\"localhost\";/$this->host=\"$DATABASE_HOST\";/g" /var/www/html/core/controller/Database.php
-apache2ctl -D FOREGROUND
