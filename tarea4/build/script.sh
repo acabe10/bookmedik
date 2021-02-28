a51c75f36e0aa7ad437e81bb9404a896001d3231
acabe10 <alejandrocabezab@gmail.com>
2021-02-28 19:17:30 +0100

    completado tarea4 a falta de plantilla volumen

--- tarea4/build/script.sh
+++ tarea4/build/script.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+cp -R /sites /var/www/html/
 mkdir -p /var/www/html/sites/default/files/translations
 chmod -R 777 /var/www/html/sites/default
 cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
