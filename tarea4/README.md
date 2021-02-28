ba769f3bd5df8f0e13d1a1e746d8f22e00cb0178
acabe10 <alejandrocabezab@gmail.com>
2021-02-28 11:49:36 +0100

    instalado drupal tarea4 correctamente sin volumen

---
+++ tarea4/README.md
@@ -0,0 +1,4 @@
+#Tarea 4: Ejecución de un CMS en dockerPermalink
+
+* A partir de una imagen base (que no sea una imagen con el CMS), genera una imagen que despliegue un CMS PHP (que no sea wordpress).
+* Crea los volúmenes necesarios para que la información que se guarda sea persistente.
---
+++ tarea4/build/script.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir -p /var/www/html/sites/default/files/translations
+chmod -R 777 /var/www/html/sites/default
+cp /var/www/html/sites/default/default.settings.php /var/www/html/sites/default/settings.php
+chmod -R 777 /var/www/html/sites/default/settings.php
+
+apache2ctl -D FOREGROUND
