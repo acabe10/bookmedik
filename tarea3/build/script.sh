796ad93f5d5f08c36eb2e96bf0348c7988d2a296
acabe10 <alejandrocabezab@gmail.com>
2021-03-02 21:06:05 +0100

    arreglando mysqli

--- tarea3/build/script.sh
+++ tarea3/build/script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-sed -i "s/$this->user=\"root\";/$this->user=\"$DATABASE_USER\";/g" /var/www/html/core/controller/Database.php
-sed -i "s/$this->pass=\"\";/$this->pass=\"$DATABASE_PASSWORD\";/g" /var/www/html/core/controller/Database.php
-sed -i "s/$this->host=\"localhost\";/$this->host=\"$DATABASE_HOST\";/g" /var/www/html/core/controller/Database.php
+cp -R /opt/bookmedik/* /code
+sed -i "s/$this->user=\"root\";/$this->user=\"$DATABASE_USER\";/g" /usr/share/nginx/html/core/controller/Database.php
+sed -i "s/$this->pass=\"\";/$this->pass=\"$DATABASE_PASSWORD\";/g" /usr/share/nginx/html/core/controller/Database.php
+sed -i "s/$this->host=\"localhost\";/$this->host=\"$DATABASE_HOST\";/g" /usr/share/nginx/html/core/controller/Database.php
+
+nginx -g 'daemon off;'
